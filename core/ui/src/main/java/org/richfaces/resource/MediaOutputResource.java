96d11b4309e8d0494d331d936a9abdc7a1a876a8
albfernandez <infjaf@gmail.com>
2016-03-07 22:13:23 +0100

    use spaces instead of tabs

--- core/ui/src/main/java/org/richfaces/resource/MediaOutputResource.java
+++ core/ui/src/main/java/org/richfaces/resource/MediaOutputResource.java
@@ -44,7 +44,7 @@ import com.google.common.base.Strings;
  */
 @DynamicResource
 public class MediaOutputResource extends AbstractUserResource implements StateHolder, CacheableResource {
-	private static final String PARENTHESES = "[^\\(]*";
+    private static final String PARENTHESES = "[^\\(]*";
     private String contentType;
     private boolean cacheable;
     private MethodExpression contentProducer;
@@ -60,11 +60,11 @@ public class MediaOutputResource extends AbstractUserResource implements StateHo
     private String fileName;
 
     public void encode(FacesContext facesContext) throws IOException {
-    	String expr = contentProducer.getExpressionString();
-    	if (!Pattern.matches(PARENTHESES, expr)) { 
-    		// method expression must not be executed
-    		throw new IllegalArgumentException("Expression \"" + expr + "\" contains parentheses.");
-    	}
+        String expr = contentProducer.getExpressionString();
+        if (!Pattern.matches(PARENTHESES, expr)) {
+            // method expression must not be executed
+            throw new IllegalArgumentException("Expression \"" + expr + "\" contains parentheses.");
+        }
         OutputStream outStream = facesContext.getExternalContext().getResponseOutputStream();
         contentProducer.invoke(facesContext.getELContext(), new Object[] { outStream, userData });
     }
